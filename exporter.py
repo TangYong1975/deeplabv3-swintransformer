2226330aeef83a913c3e3f8f72f6bc58e25d456b
TangYong <35130660+TangYong1975@users.noreply.github.com>
2021-11-25 18:10:26 +0800

    Initial commit

---
+++ exporter.py
@@ -0,0 +1,35 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+import network
+
+model = network.deeplabv3plus_swimtransformer(num_classes=2, output_stride=16)
+
+model.load_state_dict((torch.load('checkpoints/best_deeplabv3plus_swimtransformer_voc_os16.pth'))["model_state"])
+model.eval()
+
+input_image = Image.open('datasets/data/VOCdevkit/VOC2012/JPEGImages/0001-0019.jpg')
+input_image = input_image.convert("RGB")
+mytransform = transforms.Compose([
+    transforms.Resize([448, 448]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+input_tensor = mytransform(input_image)
+input_batch = input_tensor.unsqueeze(0)
+
+if torch.cuda.is_available():
+    input_batch = input_batch.to('cuda')
+    model.to('cuda')
+
+output = model(input_batch)
+print(output.shape)
+
+# NOK (swimtransformer), OK (mobilenet, resnet50)
+torch_onnx_out = torch.onnx.export(model, input_batch, 'checkpoints/best_deeplabv3plus_swimtransformer_voc_os16.onnx',
+                    export_params=True,
+                    verbose=True,
+                    input_names=['label'],
+                    output_names=["output"],
+                    opset_version=11)
---
+++ predict.py
@@ -0,0 +1,156 @@
+from torch.utils.data import dataset
+from tqdm import tqdm
+import network
+import utils
+import os
+import random
+import argparse
+import numpy as np
+
+from torch.utils import data
+from datasets import VOCSegmentation, Cityscapes, cityscapes
+from torchvision import transforms
+from metrics import StreamSegMetrics
+
+import torch
+import torch.nn as nn
+
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+from glob import glob
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+
+    # Datset Options
+    parser.add_argument("--input", type=str, default='datasets/data/VOCdevkit/VOC2012/JPEGImages', 
+                        help="path to a single image or image directory")
+    parser.add_argument("--dataset", type=str, default='voc',
+                        choices=['voc', 'cityscapes'], help='Name of training set')
+    parser.add_argument("--num_classes", type=int, default=2,
+                        help="num classes (default: None)")
+
+    # Deeplab Options
+    parser.add_argument("--model", type=str, default='deeplabv3plus_swimtransformer',
+                        choices=['deeplabv3_resnet50',  'deeplabv3plus_resnet50',
+                                 'deeplabv3_resnet101', 'deeplabv3plus_resnet101',
+                                 'deeplabv3_mobilenet', 'deeplabv3plus_mobilenet',
+                                 'deeplabv3_swimtransformer', 'deeplabv3plus_swimtransformer'], help='model name')
+    parser.add_argument("--separable_conv", action='store_true', default=False,
+                        help="apply separable conv to decoder and aspp")
+    parser.add_argument("--output_stride", type=int, default=16, choices=[8, 16])
+
+    # Train Options
+    parser.add_argument("--save_val_results_to", default="datasets/data/VOCdevkit/VOC2012/Output",
+                        help="save segmentation results to the specified dir")
+
+    parser.add_argument("--crop_val", action='store_true', default=False,
+                        help='crop validation (default: False)')
+    parser.add_argument("--val_batch_size", type=int, default=4,
+                        help='batch size for validation (default: 4)')
+    parser.add_argument("--crop_size", type=int, default=448)
+
+    
+    parser.add_argument("--ckpt", default='checkpoints/best_deeplabv3plus_swimtransformer_voc_os16.pth', type=str,
+                        help="resume from checkpoint")
+    parser.add_argument("--gpu_id", type=str, default='0',
+                        help="GPU ID")
+    return parser
+
+def main():
+    opts = get_argparser().parse_args()
+    
+    if opts.dataset.lower() == 'voc':
+        if opts.num_classes == None:
+            opts.num_classes = 21
+        decode_fn = VOCSegmentation.decode_target
+    elif opts.dataset.lower() == 'cityscapes':
+        opts.num_classes = 19
+        decode_fn = Cityscapes.decode_target
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = opts.gpu_id
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Device: %s" % device)
+
+    # Setup dataloader
+    image_files = []
+    if os.path.isdir(opts.input):
+        for ext in ['png', 'jpeg', 'jpg', 'JPEG']:
+            files = glob(os.path.join(opts.input, '**/*.'+ext), recursive=True)
+            if len(files)>0:
+                image_files.extend(files)
+    elif os.path.isfile(opts.input):
+        image_files.append(opts.input)
+    
+    # Set up model
+    model_map = {
+        'deeplabv3_resnet50': network.deeplabv3_resnet50,
+        'deeplabv3plus_resnet50': network.deeplabv3plus_resnet50,
+        'deeplabv3_resnet101': network.deeplabv3_resnet101,
+        'deeplabv3plus_resnet101': network.deeplabv3plus_resnet101,
+        'deeplabv3_mobilenet': network.deeplabv3_mobilenet,
+        'deeplabv3plus_mobilenet': network.deeplabv3plus_mobilenet,
+        'deeplabv3_swimtransformer': network.deeplabv3_swimtransformer,
+        'deeplabv3plus_swimtransformer': network.deeplabv3plus_swimtransformer
+    }
+
+    model = model_map[opts.model](num_classes=opts.num_classes, output_stride=opts.output_stride)
+    if opts.separable_conv and 'plus' in opts.model:
+        network.convert_to_separable_conv(model.classifier)
+    utils.set_bn_momentum(model.backbone, momentum=0.01)
+    
+    if opts.ckpt is not None and os.path.isfile(opts.ckpt):
+        # https://github.com/VainF/DeepLabV3Plus-Pytorch/issues/8#issuecomment-605601402, @PytaichukBohdan
+        checkpoint = torch.load(opts.ckpt, map_location=torch.device('cpu'))
+        model.load_state_dict(checkpoint["model_state"])
+        model = nn.DataParallel(model)
+        model.to(device)
+        print("Resume model from %s" % opts.ckpt)
+        del checkpoint
+    else:
+        print("[!] Retrain")
+        model = nn.DataParallel(model)
+        model.to(device)
+
+    #denorm = utils.Denormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # denormalization for ori images
+
+    if opts.crop_val:
+        transform = transforms.Compose([
+                transforms.Resize(opts.crop_size),
+                transforms.CenterCrop(opts.crop_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225]),
+            ])
+    else:
+        transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize((opts.crop_size, opts.crop_size)),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225]),
+            ])
+    transform_resize = transforms.Resize((opts.crop_size, opts.crop_size))
+
+    if opts.save_val_results_to is not None:
+        os.makedirs(opts.save_val_results_to, exist_ok=True)
+    with torch.no_grad():
+        model = model.eval()
+        for img_path in tqdm(image_files):
+            img_name = os.path.basename(img_path).split('.')[0]
+            img = Image.open(img_path).convert('RGB')
+            img2 = transform(img).unsqueeze(0) # To tensor of NCHW
+            img2 = img2.to(device)
+            
+            pred = model(img2).max(1)[1].cpu().numpy()[0] # HW
+            colorized_preds = decode_fn(pred).astype('uint8')
+            colorized_preds = Image.fromarray(colorized_preds)
+
+            img2 = transform_resize(img)
+            colorized_preds = Image.blend(img2, colorized_preds, 0.5)
+
+            if opts.save_val_results_to:
+                colorized_preds.save(os.path.join(opts.save_val_results_to, img_name+'.png'))
+
+if __name__ == '__main__':
+    main()
